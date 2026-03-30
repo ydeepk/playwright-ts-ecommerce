0b725d24aa6353f9d03b78dbb8807a989ff419d9
ydeepk <ydeepkcs@gmail.com>
2026-03-30 15:49:12 +0530

    example test - Coach gemini

---
+++ tests/live.spec.ts
@@ -0,0 +1,12 @@
+//load the tools
+import {test, expect} from '@playwright/test';
+
+//start the test
+test('Verify page title', async({page}) => {
+
+    // goto URL
+    await page.goto('https://react-shopping-cart-67954.firebaseapp.com/');
+
+    // check if page has the right title
+    await expect(page).toHaveTitle(/Typescript react shopping cart/);
+});
