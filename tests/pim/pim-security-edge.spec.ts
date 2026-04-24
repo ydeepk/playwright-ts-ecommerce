bd3a9efd6f7b0f36ee5fdacf9f607e17e807e3a1
ydeepk <ydeepkcs@gmail.com>
2026-04-25 02:23:42 +0530

    implementation: test unauthorized acess

--- tests/pim/pim-security-edge.spec.ts
+++ tests/pim/pim-security-edge.spec.ts
@@ -4,22 +4,8 @@ import * as allure from 'allure-js-commons';
 test.use({storageState: undefined});
 
 test.describe('PIM security & Edge cases', () => { 
-    
-    test.beforeEach(async({page}) => {
-        await page.goto('/');
 
-        // Defensive check: ensures session is still valid
-        // If redirected to login → storageState expired or missing
-        if (page.url().includes('auth/login')) {
-
-            // Fail fast instead of continuing with broken state
-            // Avoids misleading failures later in test steps
-            throw new Error("Session expired. Please regenerate auth state.");
-        }
-    });
-
-
-    test('verify unauthorise access', async({navbar, navigation, pimPage})=>{
+    test('Unauthorized Access to PIM Admin Features', async({page,loginPage})=>{
 
         // --- Allure Labels (non-deprecated way) ---
         await allure.label('epic', 'HR Management');
@@ -35,11 +21,61 @@ test.describe('PIM security & Edge cases', () => {
         await allure.owner('Deepak');
       
 
-        await test.step('Go to PIM page', async () => {
-               await navigation.goToPIM();
+        await test.step('Go to Login page', async() => {
+            await loginPage.navigate();
         });
 
-        
+
+        await test.step('User is redirected to login page', async() => {
+            await loginPage.redirectToLogin();
+        });
+
+
+        await test.step('Login with credentials', async() => {
+            // danny.rails , Active*89@BT
+            const nonAdminUser = 'danny.rails';
+            const nonAdminPassword = 'Active*89@BT';
+            await loginPage.login(nonAdminUser, nonAdminPassword);
+        });
+
+        await test.step('Go to PIM page', async () => {
+               await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/pim/viewEmployeeList');
+        });
+
+        await test.step('Should display Credential required', async() =>{
+            const credRequiredText = 'Credential Required';
+            await expect(page.locator('.oxd-alert--error').getByText(/credRequiredText/i)).toBeVisible();
+        });
+
+        await test.step('Should not display PIM link in Navigation', async() => {
+            await expect(page.getByRole('link', { name: 'PIM' })).toBeHidden();
+        });
+
+    });
+
+    test('verify Data Masking/Visibility', async({page,loginPage})=>{
+
+       // --- Allure Metadata ---
+        await allure.label('epic', 'HR Management');
+        await allure.label('feature', 'PIM Module');
+        await allure.story('Sensitive Data Protection');
+
+        await allure.label('severity', 'critical');
+
+        await allure.label('tag', 'security');
+        await allure.label('tag', 'access-control');
+        await allure.label('tag', 'edge-case');
+
+        await allure.label('layer', 'UI');
+
+        await allure.owner('Deepak');
+
+        // Step 1: As an Admin, create an employee with sensitive details (e.g., Date of Birth or SSN if available).
+        // Step 2: Login as a different non-admin user.
+        // Step 3: Search for that employee.
+        // Step 4: Verify that sensitive fields are either hidden, read-only, or masked (asterisks).
+      
+
 
     });
 
